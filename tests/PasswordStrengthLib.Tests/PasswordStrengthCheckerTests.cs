268ce0f621bf41b483d79de00462c78406f97aa6
John Ricks <johnricks@Johns-MacBook-Pro-2.local>
2025-08-28 11:50:49 -0400

    test: fix test file and align with library namespace/class

--- tests/PasswordStrengthLib.Tests/PasswordStrengthCheckerTests.cs
+++ tests/PasswordStrengthLib.Tests/PasswordStrengthCheckerTests.cs
@@ -1,37 +1,37 @@
 using Xunit;
-using PasswordStrengthLib;  // library namespace
+using PasswordStrengthLib;  // <- if your library namespace is different, change this
 
-public class PasswordStrengthCheckerTests
+public class PasswordEvaluatorTests
 {
     [Fact]
     public void Length_7_Fails_Length_Criterion()
     {
-        var chk = new PasswordStrengthChecker();
+        var chk = new PasswordEvaluator();
         var strength = chk.Evaluate("Aa1!aaa"); // len = 7
-        Assert.NotEqual(PasswordStrengthChecker.Strength.STRONG, strength);
+        Assert.NotEqual(PasswordEvaluator.Strength.STRONG, strength);
     }
 
     [Fact]
     public void Length_8_All_Five_Criteria_Should_Be_Strong()
     {
-        var chk = new PasswordStrengthChecker();
-        var strength = chk.Evaluate("Aa1!aaaa"); // len=8, upper/lower/digit/symbol
-        Assert.Equal(PasswordStrengthChecker.Strength.STRONG, strength);
+        var chk = new PasswordEvaluator();
+        var strength = chk.Evaluate("Aa1!aaaa"); // len = 8 + upper/lower/digit/symbol
+        Assert.Equal(PasswordEvaluator.Strength.STRONG, strength);
     }
 
     [Fact]
     public void Only_Length_Met_Should_Be_Weak()
     {
-        var chk = new PasswordStrengthChecker();
+        var chk = new PasswordEvaluator();
         var strength = chk.Evaluate("aaaaaaaa"); // only length met
-        Assert.Equal(PasswordStrengthChecker.Strength.WEAK, strength);
+        Assert.Equal(PasswordEvaluator.Strength.WEAK, strength);
     }
 
     [Fact]
     public void Two_Criteria_Should_Be_Medium()
     {
-        var chk = new PasswordStrengthChecker();
+        var chk = new PasswordEvaluator();
         var strength = chk.Evaluate("ABCDEFGH"); // length + upper = 2
-        Assert.Equal(PasswordStrengthChecker.Strength.MEDIUM, strength);
+        Assert.Equal(PasswordEvaluator.Strength.MEDIUM, strength);
     }
 }
